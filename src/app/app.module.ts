53fcde95b8dd779f4611c0c396af02441fd82848
Myron <dubtwee@gmail.com>
2020-09-10 14:34:24 -0500

    generating hero-detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     // register components/decorators
     AppComponent,
     HeroesComponent,
+    HeroDetailComponent,
   ],
   imports: [
     // register/opt-in with different Angular modules
