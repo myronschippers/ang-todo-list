c0b5d85673e7c4dc77d8505b61ec1678519670d4
Myron <dubtwee@gmail.com>
2020-09-12 22:04:43 -0500

    adding a page route for making the Heroes component its own page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // heroes page registration
+  { path: 'heroes', component: HeroesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
